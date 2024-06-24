94ebdc7e5981c5f675295fe337cd498801ffba3e
Matilda Källström <matilda.kallstrom@hotmail.se>
2024-06-24 13:55:03 +0200

    store user location

---
+++ weather-app/src/Store/useUserLocation.tsx
@@ -0,0 +1,11 @@
+import { create } from "zustand";
+
+export const useUserLocationStore = create((set) => ({
+    userLocation: {
+        latitude: 59.28,
+        longitude: 18.08
+      },
+      weatherData: {},
+    updateUserLocation: (updatedLocation: any) => set({ userLocation: updatedLocation}),
+    updateWeatherData: (data: any) => set({ weatherData: data })
+}));
