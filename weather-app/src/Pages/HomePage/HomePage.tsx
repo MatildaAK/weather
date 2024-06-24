816351567f66d61fc448a87178d4addaa670972c
Matilda Källström <matilda.kallstrom@hotmail.se>
2024-06-24 18:01:56 +0200

    styling

--- weather-app/src/Pages/HomePage/HomePage.tsx
+++ weather-app/src/Pages/HomePage/HomePage.tsx
@@ -1,16 +1,12 @@
 import SearchBar from "../../components/Search/SearchBar";
 
 const HomePage = () => {
-
-    return (
-        <>
-
-        <SearchBar />
-        <p>Hello</p>
-        
-        </>
-    )
-
-}
+  return (
+    <>
+      <SearchBar />
+      <p>Hello</p>
+    </>
+  );
+};
 
 export default HomePage;
