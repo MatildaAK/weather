cb30ae2baa50abf96f494a9bb84984fc4b68d69a
Matilda Källström <matilda.kallstrom@hotmail.se>
2024-06-24 18:01:15 +0200

    added some styling

--- weather-app/src/App.tsx
+++ weather-app/src/App.tsx
@@ -1,18 +1,18 @@
-import './App.css'
-import { Outlet } from 'react-router-dom'
+import "./App.css";
+import { Outlet } from "react-router-dom";
 
 function App() {
-
   return (
     <>
-    {/* <Header /> */}
+      {/* <Header /> */}
 
-    <main>
-      <Outlet />  
-    </main>
-
+      <main className="m-0 b-0 box-border grid">
+        <div className="min-h-screen bg-gradient-to-t from-cyan-500 via-violet-500 to-blue-500">
+          <Outlet />
+        </div>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
