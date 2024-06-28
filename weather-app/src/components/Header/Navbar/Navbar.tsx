a7838259c9c43ee7be0237ffd42b41073c02a2b8
Matilda Källström <matilda.kallstrom@hotmail.se>
2024-06-28 10:31:33 +0200

    styling

--- weather-app/src/components/Header/Navbar/Navbar.tsx
+++ weather-app/src/components/Header/Navbar/Navbar.tsx
@@ -27,17 +27,14 @@ const Navbar = () => {
       <div className="flex items-center justify-around py-6">
         {cities.map((city) => {
           return (
-            <>
               <button
                 key={city.id}
                 className="text-lg font-medium hover:bg-gray-700/20 px-3 py-2 rounded-md transition ease-in"
               >
                 {city.name}
               </button>
-            </>
           );
         })}
-        ;
       </div>
     </>
   );
--- weather-app/src/components/Search/SearchBarInput.tsx
+++ weather-app/src/components/Search/SearchBarInput.tsx
@@ -1,34 +1,23 @@
-import {
-  faLocationCrosshairs,
-  faMagnifyingGlass,
-} from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { BiCurrentLocation, BiSearch } from "react-icons/bi";
 
 const SearchBarInput = () => {
   return (
     <>
-      <div className="flex flex-row justify-center py-16">
-        <div className="flex flex-row items-center justify-center space-x-4">
+      <div className="flex flex-row justify-center my-6">
+        <div className="flex flex-row w-3/4 items-center justify-center space-x-4">
           <input
-            className="h-16 w-full border-2 border-black rounded-full text-center text-black text-xl font-bold shadow-xl capitalize focus:outline-none placeholder:lowercase"
+            className="h-16 w-full border-2 border-white rounded-md text-center text-black text-xl font-bold shadow-xl capitalize focus:outline-none placeholder:lowercase"
             type="text"
             placeholder="search by city..."
           />
-          <button type="button" className="w-16 easy-out hover:scale-125">
-            <FontAwesomeIcon
-              icon={faMagnifyingGlass}
-              size="2xl"
-              style={{ color: "#f7fafd" }}
-            />
-          </button>
-
-          <button type="button" className="w-16 easy-out hover:scale-125">
-            <FontAwesomeIcon
-              icon={faLocationCrosshairs}
-              size="2xl"
-              style={{ color: "#f7fafd" }}
-            />
-          </button>
+          <BiSearch
+            size={40}
+            className="cursor-pointer transition ease-out hover:scale-125"
+          />
+          <BiCurrentLocation
+            size={40}
+            className="cursor-pointer transition ease-out hover:scale-125"
+          />
         </div>
       </div>
     </>
