8482990b0a8d402fda73b6a6c3de52076e194193
Matilda Källström <matilda.kallstrom@hotmail.se>
2024-06-28 13:51:07 +0200

    3 h forecast

--- weather-app/src/components/TodaysWeather/TodaysWeather.tsx
+++ weather-app/src/components/TodaysWeather/TodaysWeather.tsx
@@ -1,10 +1,33 @@
 const TodaysWeather = () => {
-    
-    return (
-        <>
-        <p>Hourly Forecast</p>
-        </>
-    )
+  const data = [1, 2, 3, 4, 5];
+  return (
+    <>
+      <div>
+        <div className="flex items-center justify-start mt-6">
+          <p className="font-medium uppercase">3 hour forecast </p>
+        </div>
+        <hr className="my-1" />
+        <div className="flex items-center justify-between">
+          {data.map((data, index) => {
+            return (
+              <div
+                key={index}
+                className="flex flex-col items-center justify-center"
+              >
+                <p className="font-light text-sm">Wed</p>
+                <img
+                  src="http://openweathermap.org/img/wn/01d@2x.png"
+                  alt="Weather icon"
+                  className="w-12 my-1"
+                />
+                <p className="font-medium">12</p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default TodaysWeather;
